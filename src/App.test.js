3ca6a37b98fe790c5921961f7de985f851dac485
RoseChilds <rosechilds09@gmail.com>
2021-12-19 16:08:34 +0000

    Fix test name

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders hello world', () => {
   render(<App />);
   const title = screen.getByText(/hello world/i);
   expect(title).toBeInTheDocument();
