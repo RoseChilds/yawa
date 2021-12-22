91321a4f821aed9666a3e8b84c9ddcd2876d6db3
LeVonara <malkawitalal@gmail.com>
2021-12-22 14:04:05 +0100

    Chore: Add prettier format

--- src/About.js
+++ src/About.js
@@ -1,13 +1,13 @@
-import {Component} from "react";
+import { Component } from "react";
 
-class About extends Component{
-    render(){
-        return (
-            <div className={"about"}>
-                <h1>About</h1>
-            </div>
-        )
-    }
+class About extends Component {
+  render() {
+    return (
+      <div className={"about"}>
+        <h1>About</h1>
+      </div>
+    );
+  }
 }
 
 export default About;
