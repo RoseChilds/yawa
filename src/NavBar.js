ab55b962abdc3e32368ba436638a7201d18bd162
LeVonara <malkawitalal@gmail.com>
2021-12-22 20:56:56 +0100

    Chore: Cleaning messy HTML attributes

--- src/NavBar.js
+++ src/NavBar.js
@@ -8,22 +8,20 @@ function Link(props) {
 class NavBar extends Component {
   render() {
     return (
-      <div className={"navbar"}>
-        <h1 className={"site-name"}>YAWA</h1>
-        <ul className={"nav-container"}>
-          <li>
-            <Link to={"/"}>Home</Link>
-          </li>
-          <li>
-            <Link to={"/weather"}>Weather</Link>
-          </li>
-          <li>
-            <Link to={"/about"}>About</Link>
-          </li>
-          <li>
-            <Link to={"/credits"}>Credits</Link>
-          </li>
-        </ul>
+      <div className="navbar">
+        <h1>YAWA</h1>
+        <span>
+          <Link to={"/"}>Home</Link>
+        </span>
+        <span>
+          <Link to={"/weather"}>Weather</Link>
+        </span>
+        <span>
+          <Link to={"/about"}>About</Link>
+        </span>
+        <span>
+          <Link to={"/credits"}>Credits</Link>
+        </span>
       </div>
     );
   }
