6d78e14fb7a825e40057253ca596be9670e8f686
RoseChilds <rosechilds09@gmail.com>
2021-12-22 21:42:39 +0000

    Remove unneeded links + make it so you can click the background

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,5 +1,5 @@
-import { NavLink } from "react-router-dom";
-import { Component } from "react";
+import {NavLink} from "react-router-dom";
+import {Component} from "react";
 
 function Link(props) {
   return <NavLink exact {...props} activeClassName="active" />;
@@ -12,17 +12,11 @@ class NavBar extends Component {
         <h3>YAWA</h3>
         <div className="navbar-main">
           <span>
-            <Link to={"/about"}>About</Link>
-          </span>
-          <span>
-            <Link to={"/credits"}>Credits</Link>
-          </span>
-          <span>
             <Link to={"/"}>Home</Link>
           </span>
         </div>
-        <span className="weather">
-          <Link to={"/weather"}>Weather</Link>
+        <span>
+          <Link to={"/weather"} className="weather">Weather</Link>
         </span>
       </div>
     );
