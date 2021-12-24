a9a83b798b4f1493f97233a5cbf81e27a37c05ce
LeVonara <malkawitalal@gmail.com>
2021-12-24 15:44:20 +0100

    Err: Css grid wip (background color change)

--- src/home-page.js
+++ src/home-page.js
@@ -4,17 +4,10 @@ class HomePage extends Component {
   render() {
     return (
       <div className="intro-wrapper">
-        <h2>
-          <p>Yet Another</p>
-          <p>Weather Application</p>
-        </h2>
-        <p>but clean and aesthetic for the sanity of your eyes</p>
-        {/* Putting our avatar here would be a nice addition */}
-        <p>RoseChilds and LeVonara</p>
-        <h2>
-          <p>Developed by ❤️</p>
-          <p>with these people</p>
-        </h2>
+        <div className="cell">a</div>
+        <div>a</div>
+        <div>a</div>
+        <div>a</div>
       </div>
     );
   }
